2255029cc91b9e9f6d53ef904b4c2d2ad3c13d16
Andrieu, Alban <alban.andrieu@free.fr>
2015-03-19 11:39:54 +0100

    Import a basic yo angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cmrTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cmrTestApp
+ */
+angular.module('cmrTestApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
