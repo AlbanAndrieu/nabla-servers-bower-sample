b6c9f953d45ff59951fe8bb8945d28ba1fc6027b
Andrieu, Alban <alban.andrieu@free.fr>
2015-08-25 14:09:07 +0200

    Disable remote webdriver

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -3,8 +3,7 @@
 exports.config = {
   // Specify you want to use jasmine 2.x as you would with mocha and cucumber.
   //framework: 'jasmine2',
-  seleniumAddress: 'http://home.nabla.mobi:4444/wd/hub',
-  //seleniumAddress: 'http://kgrdb01:4444/wd/hub',
+  //seleniumAddress: 'http://home.nabla.mobi:4444/wd/hub',
   specs: ['e2e/example/*_test.js'],
   //baseUrl: 'http://' + process.env.SERVER_HOST + ':' + process.env.JETTY_PORT,
   baseUrl: 'http://home.nabla.mobi:' + process.env.JETTY_PORT || 9090,
@@ -22,7 +21,7 @@ exports.config = {
     proxy: {
        proxyType: 'manual',
        //httpProxy: 'localhost:' + process.env.ZAP_PORT,
-       httpProxy: 'home.nabla.mobi:' + process.env.ZAP_PORT || 8090,
+       httpProxy: 'localhost:' + process.env.ZAP_PORT || 8090,
        sslProxy: '',
        noProxy: ''
     }
