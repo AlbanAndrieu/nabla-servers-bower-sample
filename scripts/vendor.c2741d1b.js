d3cc72262f793ee609f09c2b612eb6cc050fdc66
AlbanAndrieu <alban.andrieu@free.fr>
2015-11-17 01:43:12 +0100

    Updates

--- scripts/vendor.c2741d1b.js
+++ scripts/vendor.c2741d1b.js
@@ -1,6 +1,6 @@
 /**
  * bower-sample
- * @version v1.0.1 - 2015-11-16
+ * @version v1.0.1 - 2015-11-17
  * @link http://home.nabla.mobi
  * @author Alban Andrieu <alban.andrieu@nabla.mobi>
  * @license MIT License, http://www.opensource.org/licenses/MIT
