5f8389abc689c7f0dd4771e0d64e624b2aa0197c
AlbanAndrieu <alban.andrieu@free.fr>
2015-12-29 16:57:08 +0100

    Updates

--- styles/main.02594f24.css
+++ styles/main.02594f24.css
@@ -1,6 +1,6 @@
 /**
  * bower-sample
- * @version v1.0.1 - 2015-11-26
+ * @version v1.0.1 - 2015-12-29
  * @link http://home.nabla.mobi
  * @author Alban Andrieu <alban.andrieu@nabla.mobi>
  * @license MIT License, http://www.opensource.org/licenses/MIT
