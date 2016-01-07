6a1cb2d94fe15475f20858a5ae710c1c9cbdb4b2
Andrieu, Alban <alban.andrieu@free.fr>
2016-01-07 07:30:48 -0600

    Upgrade dependencies

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-REM OK TO RUN SERVER mvn clean install org.codehaus.cargo:cargo-maven2-plugin:run -Dserver=jetty9x > deploy.log 2>&1
+REM OK TO RUN SERVER mvn clean install org.codehaus.cargo:cargo-maven2-plugin:run -Dserver=jetty9x -Pjacoco > deploy.log 2>&1
