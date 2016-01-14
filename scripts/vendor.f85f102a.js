e26080529f154d131c4de24db084a3a38577cd62
AlbanAndrieu <alban.andrieu@free.fr>
2016-01-14 02:46:57 +0100

    Updates

--- scripts/vendor.f85f102a.js
+++ scripts/vendor.f85f102a.js
@@ -1,6 +1,6 @@
 /**
  * bower-sample
- * @version v1.0.1 - 2016-01-13
+ * @version v1.0.1 - 2016-01-14
  * @link http://home.nabla.mobi
  * @author Alban Andrieu <alban.andrieu@nabla.mobi>
  * @license MIT License, http://www.opensource.org/licenses/MIT
