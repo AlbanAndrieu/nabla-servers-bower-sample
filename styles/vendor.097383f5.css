e2b47cdfc7e5d5c6ccbbd241a117e2ba0633f7dd
AlbanAndrieu <alban.andrieu@free.fr>
2016-01-19 06:55:14 +0100

    Updates

--- styles/vendor.097383f5.css
+++ styles/vendor.097383f5.css
@@ -1,6 +1,6 @@
 /**
  * bower-sample
- * @version v1.0.1 - 2016-01-18
+ * @version v1.0.1 - 2016-01-19
  * @link http://home.nabla.mobi
  * @author Alban Andrieu <alban.andrieu@nabla.mobi>
  * @license MIT License, http://www.opensource.org/licenses/MIT
