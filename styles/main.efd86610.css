ee43f8b6d7ec7e31f5f3ef9b2ccb085c7ec903b5
AlbanAndrieu <alban.andrieu@free.fr>
2016-01-21 07:04:33 +0100

    Updates

--- styles/main.efd86610.css
+++ styles/main.efd86610.css
@@ -1,6 +1,6 @@
 /**
  * bower-sample
- * @version v1.0.1 - 2016-01-19
+ * @version v1.0.1 - 2016-01-21
  * @link http://home.nabla.mobi
  * @author Alban Andrieu <alban.andrieu@nabla.mobi>
  * @license MIT License, http://www.opensource.org/licenses/MIT
