a097d59dfe3a44afdf2ab6ddd64012d8e10764e0
Andrieu, Alban <alban.andrieu@misys.com>
2017-01-05 23:15:24 +0100

    selenium update

--- run.bat
+++ run.bat
@@ -1,1 +1,14 @@
 REM OK TO RUN SERVER mvn clean install org.codehaus.cargo:cargo-maven2-plugin:run -Dserver=jetty9x -Pjacoco > deploy.log 2>&1
+
+grunt phantomflow:websitetests --report
+
+sudo apt-get install graphicsmagick
+
+# examples/wraith
+gem install wraith
+
+# run the capture process
+wraith capture demo
+
+# view results in the browser
+open shots/gallery.html
