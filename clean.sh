51385a34ad9bd90e1e64f1d42533a85cd768538a
Andrieu, Alban <alban.andrieu@misys.com>
2018-01-26 18:04:17 +0100

    Upgrade protractor and webdriver

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xv
 
-rm -Rf .node_cache/ .node_tmp/  .tmp/ .bower/ node node_modules/ package-lock.json yarn.lock
+rm -Rf .node_cache/ .node_tmp/  .tmp/ .bower/ bower_components/ node node_modules/ .sass-cache/ package-lock.json yarn.lock target/
 
 exit 0
