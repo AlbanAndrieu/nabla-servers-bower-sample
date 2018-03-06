74b294d01f72280184718b5d6da18aa05dbb18be
AlbanAndrieu <alban.andrieu@free.fr>
2018-03-06 22:29:05 +0100

    Updates

--- styles/vendor.0c028814.css
+++ styles/vendor.0c028814.css
@@ -1,6 +1,6 @@
 /**
  * bower-sample
- * @version v1.0.2 - 2018-03-05
+ * @version v1.0.2 - 2018-03-06
  * @link http://home.nabla.mobi
  * @author Alban Andrieu alban.andrieu@nabla.mobi
  * @license , 
