0d8c292a216b3496e381c306b67ee75a5582e844
Andrieu, Alban <alban.andrieu@free.fr>
2018-07-23 22:08:13 +0200

    Use mvnw

--- clean.sh
+++ clean.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -xv
 
-rm -Rf npm/ .node_cache/ .node_tmp/  .tmp/ .bower/ bower_components/ node node_modules/ .sass-cache/ package-lock.json yarn.lock target/ build/phantomas/ dist/ docs/groovydocs/ site/ coverage/
+rm -Rf npm/ .node_cache/ .node_tmp/  .tmp/ .bower/ bower_components/ node node_modules/ .sass-cache/ package-lock.json yarn.lock target/ build/ dist/ groovydocs/ site/ coverage/
+#/phantomas/ docs/
 #dist/bower_components/ dist/fonts/
 
 exit 0
