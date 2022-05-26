6b4668b14baea5d86ab1e4c2c57db737051a1ab7
Andrieu, Alban <alban.andrieu@free.fr>
2022-05-26 13:35:51 +0200

    Tag 1.0.4

--- webdriver.sh
+++ webdriver.sh
@@ -8,7 +8,7 @@ source "${WORKING_DIR}/scripts/step-0-color.sh"
 
 export WEBDRIVER_CHROME_VERSION=${WEBDRIVER_CHROME_VERSION:-"101.0.4951.64"}
 
-echo -e "${magenta} npm install -g bower@1.8.13 grunt-cli@1.4.3 webdriver-manager@12.1.8 ${NC}"
+echo -e "${magenta} npm install -g bower@1.8.13 grunt@1.5.3 webdriver-manager@12.1.8 ${NC}"
 
 # NOK ./node_modules/webdriver-manager/bin/webdriver-manager update --versions.chrome ${WEBDRIVER_CHROME_VERSION}
 echo -e "${magenta} ./node_modules/grunt-protractor-runner/node_modules/protractor/bin/webdriver-manager update --versions.chrome ${WEBDRIVER_CHROME_VERSION} ${NC}"
