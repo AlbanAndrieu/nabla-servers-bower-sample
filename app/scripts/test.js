346b63fdf5635e6e8cf1853be220c2bfe96d2d2b
AlbanAndrieu <alban.andrieu@free.fr>
2023-08-25 17:42:55 +0200

    Add mixpanel

--- app/scripts/test.js
+++ app/scripts/test.js
@@ -1,2 +1,6 @@
 /*jshint strict: false */
+import mixpanel from 'mixpanel-browser';
+
+mixpanel.init("e28ee9625ec33982c6f8666ed974382c", { debug: true, ignore_dnt: true, track_pageview: true, persistence: "localStorage" });
+
 console.log("'Allo 'Allo!");
