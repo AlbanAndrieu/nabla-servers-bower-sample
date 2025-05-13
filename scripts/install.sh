e515bde8f02972e70c24fb600d6ce5644615c85f
AlbanAndrieu <alban.andrieu@free.fr>
2025-05-13 19:22:10 +0200

    ansible-core upgrade

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,10 +11,16 @@ echo -e "${magenta} sudo apt install chromium-chromedriver ${NC}"
 
 nvm install 20.19.1
 
+npm install -g grunt@1.5.3
+npm install -g grunt-cli@1.4.3
+npm install -g bower@1.8.13
+# npm install -g bower-nexus3-resolver@1.0.4
+
 gem cleanup all
 
 # shellcheck disable=SC2154
 echo -e "${magenta} sudo gem un install compass"
+echo -e "${magenta} direnv allow"
 gem install sass compass
 
 echo -e "${green} Installing DONE. ${NC}"
