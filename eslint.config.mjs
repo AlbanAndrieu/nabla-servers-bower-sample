c9a19e285dc21aec68f485e369bd990aeac1cee4
AlbanAndrieu <alban.andrieu@free.fr>
2025-06-13 19:00:39 +0200

    npx eslint --format gitlab

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,66 +8,68 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
 export default [{
-    ignores: [
-        "!**/.eslintrc.js",
-        ".direnv/*",
-        ".venv/*",
-        "__tests__/*",
-        "test/*",
-        "docs/*",
-        "**/netsniff.js",
-        "**/rasterize.js",
-        "app/scripts/test/*",
-        "report/*",
-        "node_modules/*",
-        "bower_components/*",
-        "megalinter-reports/*",
-        "target/*",
-        "dist/*",
-        "bin/*",
-    ],
+  ignores: [
+      "!**/.eslintrc.js",
+      ".direnv/*",
+      ".venv/*",
+      ".bower/*",
+      "__tests__/*",
+      "test/*",
+      "docs/*",
+      "**/netsniff.js",
+      "**/rasterize.js",
+      "app/scripts/test/*",
+      "report/*",
+      "node_modules/*",
+      "bower_components/*",
+      "megalinter-reports/*",
+      "target/*",
+      "dist/*",
+      "bin/*",
+      "Gruntfile.js",
+  ],
 }, ...compat.extends("eslint:recommended"), {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
+  plugins: {
+      "@typescript-eslint": typescriptEslint,
+  },
 
-    languageOptions: {
-        globals: {
-            ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, "off"])),
-            ...globals.node,
-            ...globals.commonjs,
-            angular: true,
-            _: "readonly",
-        },
+  languageOptions: {
+      globals: {
+          ...Object.fromEntries(Object.entries(globals.browser).map(([key]) => [key, "off"])),
+          ...globals.node,
+          ...globals.commonjs,
+          angular: true,
+          _: "readonly",
+      },
 
-        ecmaVersion: "latest",
-        sourceType: "module",
+      ecmaVersion: "latest",
+      sourceType: "module",
 
-        parserOptions: {
-            parser: "@typescript-eslint/parser",
-        },
-    },
+      parserOptions: {
+          parser: "@typescript-eslint/parser",
+      },
+  },
 
-    rules: {
-        indent: ["error", 2],
-        semi: ["error", "always"],
-        quotes: ["error", "double"],
-    },
+  rules: {
+      indent: ["error", 2],
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+  },
 }, {
-    files: ["bin/*.js", "lib/*.js"],
-    ignores: ["**/*.test.js"],
+  files: ["bin/*.js", "lib/*.js"],
+  ignores: ["**/*.test.js"],
 
-    rules: {
-        indent: ["error", 2],
+  rules: {
+      indent: ["error", 2],
 
-        quotes: ["error", "double", {
-            avoidEscape: true,
-        }],
-    },
+      quotes: ["error", "double", {
+          avoidEscape: true,
+      }],
+  },
 }];
